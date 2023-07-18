3bbbd6d794cbea461a4fb426d89fb395f3b8cdc4
omartorres28 <omartorres@colegioguanenta.edu.co>
2023-07-18 06:42:58 -0500

    commit

---
+++ 3_operadores.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Operadores</h1>
+    <?php
+    //Operadores aritmético//
+    echo (5 % 3)."\n";           // muestra 2
+    echo (5 % -3)."\n";          // muestra 2
+    echo (-5 % 3)."\n";          // muestra -2
+    echo (-5 % -3)."\n";         // muestra -2
+    //Operadores logicos//
+
+    //operadores en cadena//
+
+</body>
+</html>
